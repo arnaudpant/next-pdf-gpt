0c7934d8c85721156650b86e904d0121439cd6c1
macbook kal <arnaud.pantenier@gmail.com>
2024-04-17 21:21:18 +0200

    PAGE Fin page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,14 @@ export default function Home() {
             <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
                 <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop:blur transition-all hover:border-gray-300 hover:bg-white/50">
                     <p className="text-sm font-semibold text-gray-700">
-                        pdfGPT est en ligne
+                        PdfGPT est en ligne
                     </p>
                 </div>
                 <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
                     Tchat avec ton <span className="text-orange-600">PDF</span>
                 </h1>
                 <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
-                    pdfGPT vous permets de discuter avec n&apos; importe quel
+                    PdfGPT vous permets de discuter avec n&apos; importe quel
                     document PDF. Téléchargez votre document et commencez à lui
                     poser des questions.
                 </p>
@@ -39,26 +39,104 @@ export default function Home() {
                         aria-hidden="true"
                         className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
                     >
-                        <div
-                            style={{
-                                clipPath:
-                                    "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-                            }}
-                            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9098fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-                        ></div>
+                    </div>
+                    <div>
+                        <div className="mx-auto max-w-6xl px-6 lg:px-8">
+                            <div className="mt-16 flow-root sm:mt-24">
+                                <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                                    <Image
+                                        src="/dashboard-preview.jpg"
+                                        alt="product preview"
+                                        width={1364}
+                                        height={866}
+                                        quality={100}
+                                        className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        aria-hidden="true"
+                        className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+                    >
                     </div>
                 </div>
             </div>
-                            {/* 53min */}
-            <div>
+            {/* Feature section */}
+            <div className="mx-auto mb-32 mt-32 max-w-5xl sm:mt-56">
+                <div className="mb-12 px-6 lg:px-8">
+                    <div className="mx-auto max-w-2xl sm:text-center">
+                        <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">
+                            Commencez le tchat dans quelques minutes
+                        </h2>
+                        <p className="mt-4 text-lg text-gray-600">
+                            Tchatter avec votre fichier PDF n&apos;a jamais été
+                            aussi simple avec PdfGPT.
+                        </p>
+                    </div>
+                </div>
+                {/* steps */}
+                <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
+                    <li className="md:flex-1">
+                        <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+                            <span className="text-sm font-medium text-blue-600">
+                                Etape 1
+                            </span>
+                            <span className="text-xl font-semibold">
+                                Créer un compte
+                            </span>
+                            <span className="mt-2 text-zinc-700">
+                                Soit vous commencez avec un forfait gratuit,
+                                soit vous choisissez notre{" "}
+                                <Link
+                                    href="/pricing"
+                                    className="text-blue-700 underline underline-offset-2"
+                                >
+                                    plan pro
+                                </Link>
+                                .
+                            </span>
+                        </div>
+                    </li>
+                    <li className="md:flex-1">
+                        <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+                            <span className="text-sm font-medium text-blue-600">
+                                Etape 2
+                            </span>
+                            <span className="text-xl font-semibold">
+                                Téléchargez votre fichier PDF
+                            </span>
+                            <span className="mt-2 text-zinc-700">
+                                Nous traiterons votre dossier et le préparerons
+                                pour que vous puissiez discuter avec lui.
+                            </span>
+                        </div>
+                    </li>
+                    <li className="md:flex-1">
+                        <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+                            <span className="text-sm font-medium text-blue-600">
+                                Etape 3
+                            </span>
+                            <span className="text-xl font-semibold">
+                                Commencez à poser des questions
+                            </span>
+                            <span className="mt-2 text-zinc-700">
+                                C&apos;est si simple. Essayez PdfGPT
+                                aujourd&apos;hui – cela prend vraiment moins
+                                d&apos;une minute.
+                            </span>
+                        </div>
+                    </li>
+                </ol>
                 <div className="mx-auto max-w-6xl px-6 lg:px-8">
                     <div className="mt-16 flow-root sm:mt-24">
                         <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
                             <Image
-                                src="/dashboard-preview.jpg"
-                                alt="product preview"
-                                width={1364}
-                                height={866}
+                                src="/file-upload-preview.jpg"
+                                alt="uploading preview"
+                                width={1419}
+                                height={732}
                                 quality={100}
                                 className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
                             />
