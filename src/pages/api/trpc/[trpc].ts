b3b2858813c0a4a672d6dbd21ae72d33e4bdb055
macbook kal <arnaud.pantenier@gmail.com>
2024-05-14 22:10:50 +0200

    reboot

--- src/pages/api/trpc/[trpc].ts
+++
@@ -1,8 +0,0 @@
-import { createNextApiHandler } from '@trpc/server/adapters/next';
-import { createContext } from '../../../server/trpc/context';
-import { appRouter } from '../../../server/trpc/router/_app';
-// @link https://nextjs.org/docs/api-routes/introduction
-export default createNextApiHandler({
-    router: appRouter,
-    createContext,
-});
