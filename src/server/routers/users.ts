9a81f06b5a014e7bff9d8a6fc512f5818934a82d
macbook kal <arnaud.pantenier@gmail.com>
2024-05-17 18:00:37 +0200

    Suppression com

--- src/server/routers/users.ts
+++ src/server/routers/users.ts
@@ -7,58 +7,10 @@ const prisma = new PrismaClient();
 
 export const usersRouter = router({
     get: publicProcedure.query(async () => {
+
+        
+
         return prisma.user.findMany()
     }),
-    // create: publicProcedure
-    //     .input(
-    //         z.object({
-    //             id: z.number(),
-    //             name: z.string(),
-    //             email: z.string(),
-    //         })
-    //     )
-    //     .mutation(async (opts) => {
-    //         const { input } = opts;
-    //         await prisma.user.create({
-    //             data: {
-    //                 id: input.id,
-    //                 name: input.name,
-    //                 email: input.email,
-    //             },
-    //         });
-    //     }),
-    // update: publicProcedure
-    //     .input(
-    //         z.object({
-    //             id: z.number(),
-    //             name: z.string(),
-    //             email: z.string(),
-    //         })
-    //     )
-    //     .mutation(async (opts) => {
-    //         const { input } = opts;
-    //         await prisma.user.update({
-    //             where: {
-    //                 id: input.id,
-    //             },
-    //             data: {
-    //                 name: input.name,
-    //                 email: input.email,
-    //             },
-    //         });
-    //     }),
-    // delete: publicProcedure
-    //     .input(
-    //         z.object({
-    //             id: z.number(),
-    //         })
-    //     )
-    //     .mutation(async (opts) => {
-    //         const { input } = opts;
-    //         await prisma.user.delete({
-    //             where: {
-    //                 id: input.id,
-    //             },
-    //         });
-    //     }),
+   
 });
